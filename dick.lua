8d301dc0861b0e6d8767222dcf52bd6dea1b521d
Doctor Internet <jonjon1234.github@gmail.com>
2015-07-30 17:24:22 +0100

    Update

--- dick.lua
+++ dick.lua
@@ -1,6 +1,6 @@
-hook.Add("think", "enzyme_dick", function()
+hook.Add("Think", "enzyme_dick", function()
 	for _, v in pairs(player.GetAll())
-		if v:SteamID == "STEAM_0:0:31154034" then
+		if v:SteamID == "STEAM_0:0:31154034" then --[FL] Enzyme
 			v:ChatPrint("dickbutt")
 		end
 	end
