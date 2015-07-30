e0e45b19b3d59f0f9294e884e6c551d2bedca9d7
Doctor Internet <jonjon1234.github@gmail.com>
2015-07-30 17:11:43 +0100

    Replaced the blank steamID with the steamID of the target.

--- dick.lua
+++ dick.lua
@@ -1,6 +1,6 @@
 hook.Add("think", "enzyme_dick", function()
 	for _, v in pairs(player.GetAll())
-		if v:SteamID == "" then
+		if v:SteamID == "STEAM_0:0:31154034" then
 			v:ChatPrint("dickbutt")
 		end
 	end
